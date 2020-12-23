73aba2228778ab14c2f5adb819c51fb0f635b3c0
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-23 01:56:29 +0000

    updated name for package dmpkg.funs

--- data-raw/include_boundaries.R
+++ data-raw/include_boundaries.R
@@ -5,10 +5,10 @@
 bnames <- levels(dmpkg.geouk::location_types$location_type)
 
 for(bn in bnames){
-    if(file.exists(file.path(popiFun::bnduk_spath, bn))){
+    if(file.exists(file.path(dmpkg.funs::bnduk_spath, bn))){
         message('Processing <', bn, '>')
         message(' * reading...')
-        assign(bn, readRDS(file.path(popiFun::bnduk_spath, bn)))
+        assign(bn, readRDS(file.path(dmpkg.funs::bnduk_spath, bn)))
         message(' * saving shapefile...')
         rgdal::writeOGR(get(bn), dsn = 'data-raw', layer = bn, driver = 'ESRI Shapefile', overwrite_layer = TRUE)
         message(' * saving compressed rda...')
