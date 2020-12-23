f49027760e8c38f5d3bfaa45ec070e37070bf979
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-23 01:05:22 +0000

    first commit

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' @import sp
+NULL
+
+#' OA. Boundaries for Output Areas in UK with geographical coordinates and
+#'
+#' @format A SpatialPolygonsDataFrame with an \code{id} column
+#'
+#' To add data load either the package \url{} or \url{}
+#'
+#' For further details on the UK Geographies, see \url{http://}
+#'
+'OA'
+
+#' LSOA. Boundaries for Output Areas in UK with geographical coordinates and
+#'
+#' @format A SpatialPolygonsDataFrame with an \code{id} column
+#'
+#' To add data load either the package \url{} or \url{}
+#'
+#' For further details on the UK Geographies, see \url{http://}
+#'
+'LSOA'
+
+#' MSOA. Boundaries for Output Areas in UK with geographical coordinates and
+#'
+#' @format A SpatialPolygonsDataFrame with an \code{id} column
+#'
+#' To add data load either the package \url{} or \url{}
+#'
+#' For further details on the UK Geographies, see \url{http://}
+#'
+'MSOA'
+
---
+++ data-raw/include_boundaries.R
@@ -0,0 +1,21 @@
+#########################################################################################
+# dmpkg.geouk * Boundaries UK * Copy boundaries from PUBLIC repo to package DATA subdir #
+#########################################################################################
+
+bnames <- levels(dmpkg.geouk::location_types$location_type)
+
+for(bn in bnames){
+    if(file.exists(file.path(popiFun::bnduk_spath, bn))){
+        message('Processing <', bn, '>')
+        message(' * reading...')
+        assign(bn, readRDS(file.path(popiFun::bnduk_spath, bn)))
+        message(' * saving shapefile...')
+        rgdal::writeOGR(get(bn), dsn = 'data-raw', layer = bn, driver = 'ESRI Shapefile', overwrite_layer = TRUE)
+        message(' * saving compressed rda...')
+        save(list = bn, file = file.path('data', paste0(bn, '.rda')), version = 3, compress = 'gzip')
+    } else{
+        message('Boundaries for <', bn, '> not found!')
+    }
+}
+
+rm(list = c(bnames, 'bn', 'bnames'))
