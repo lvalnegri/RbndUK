24ed24b12199fb31aa2f12011d1737afdcee4e47
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-09-10 15:01:01 +0100

    commit

--- R/include_outputs.R
+++ R/include_outputs.R
@@ -2,15 +2,15 @@
 # dmpkg.bnduk * Boundaries UK * Copy csv outputs of the aggregation process from PUBLIC repo to package DATA-RAW subdir #
 #########################################################################################################################
 
-bnames <- dmpkg.geouk::location_types[, .(location_type, max_child)]
-bnames <- bnames[!is.na(max_child)]
+bnames <- dmpkg.geouk::location_types[, c('location_type', 'max_child')]
+bnames <- bnames[!is.na(bnames$max_child), ]
 
-for(bn in bnames){
-    bname <- paste0(bn$max_child, '_to_', bn$location_type, '.csv')
-    if(file.exists(file.path(dmpkg.funs::bnduk_spath, bn))){
-        message('Processing <', bn, '>')
+for(idx in 1:nrow(bnames)){
+    bname <- paste0(bnames[idx, 'max_child'], '_to_', bnames[idx, 'location_type'], '.csv')
+    if(file.exists(file.path(dmpkg.funs::bnduk_spath, bnames[idx, 'max_child']))){
+        message('Processing <', bnames[idx, 'max_child'], '>')
         file.copy( file.path(dmpkg.funs::ext_path, 'uk', 'geography', 'lookups', bname), file.copy('data-raw', bname) )
     } else{
-        message('File not present for <', bn[1], '>!')
+        message('File not present for <',bnames[idx, 'max_child'], '>!')
     }
 }
